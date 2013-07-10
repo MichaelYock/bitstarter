03ca80d733d17f70f4f642059fb428b142f21559
MichaelYock <michael.yockney@gmail.com>
2013-07-10 13:15:53 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs =require('fs');
 
 var app = express.createServer(express.logger());
 
+var message = fs.readFileSync(index.html,utf8 );
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(message);
+  console.log(message);
 });
 
 var port = process.env.PORT || 5000;
