78df537c6d662f7d5b248759c2972ebcdd7fb981
Michael Yockney <Michael.yockney@gmail.com>
2013-07-11 15:31:15 +0700

    Update web.js

--- web.js
+++ web.js
@@ -1,13 +1,17 @@
 var express = require('express');
-var fs =require('fs');
+var fs = require('fs');
+var FILE_DEFAULT = "index.html";
 
 var app = express.createServer(express.logger());
 
-var message = fs.readFileSync(index.html,utf8 );
-
+var message = function (file) {
+  file = file || FILE_DEFAULT;
+	var buffer = new Buffer(fs.readFileSync(file));
+	return (buffer.toString());
+}
+	
 app.get('/', function(request, response) {
-  response.send(message);
-  console.log(message);
+  response.send(message());
 });
 
 var port = process.env.PORT || 5000;
